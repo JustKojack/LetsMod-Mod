d2dab73ba9d569736d2e3f0849d11450c623f790
JustKojack <jellojunky44@gmail.com>
2013-05-10 19:00:07 -0700

    Test

---
+++ letsmod-common/kjck/mods/letsmod/letsMod.java
@@ -0,0 +1,9 @@
+package kjck.mods.letsmod;
+
+import kjck.mods.letsmod.lib.reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = reference.MOD_ID, name = reference.MOD_NAME, version = reference.VERSION)
+public class letsMod {
+
+}
---
+++ letsmod-common/kjck/mods/letsmod/lib/reference.java
@@ -0,0 +1,11 @@
+package kjck.mods.letsmod.lib;
+
+public class reference {
+
+    public static final String MOD_ID = "letsMod";
+    public static final String MOD_NAME = "Let's Mod";
+    public static final String VERSION = "0.0.1";
+            
+    
+    
+}
