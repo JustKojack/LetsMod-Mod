39606b96cb458c229066f4d1fbdaa29398591888
JustKojack <jellojunky44@gmail.com>
2013-05-11 21:51:34 -0700

    Initial Commit

--- letsmod-common/kjck/mods/letsmod/letsMod.java
+++ letsmod-common/kjck/mods/letsmod/letsMod.java
@@ -2,8 +2,32 @@ package kjck.mods.letsmod;
 
 import kjck.mods.letsmod.lib.reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 
 @Mod(modid = reference.MOD_ID, name = reference.MOD_NAME, version = reference.VERSION)
+
 public class letsMod {
 
+    @PreInit
+    public void PreInit(FMLPreInitializationEvent event){
+        
+    }
+    
+    @Init
+    public void load(FMLInitializationEvent event){
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event){
+        
+        
+    }
+    
 }
